b54b07e7b46123516204428456a53c6f62fd277e
bulabu <mukul.soundarajan@avisocoaching.com>
2018-02-09 22:38:22 -0500

    Added template question parsing and webpack build

--- js/main.js
+++ js/main.js
@@ -1,16 +1,68 @@
+import Docxtemplater from "docxtemplater"
+import JSZip from "jszip"
+
+class InspectModule {
+	constructor() {
+		this.inspect = {};
+		this.fullInspected = {};
+		this.filePath = null;
+	}
+	set(obj) {
+		if (obj.inspect) {
+			if (obj.inspect.filePath) {
+				this.filePath = obj.inspect.filePath;
+			}
+			this.inspect = Object.assign(this.inspect, obj.inspect);
+			this.fullInspected[this.filePath] = this.inspect;
+		}
+	}
+}
+
+var state = {};
 function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-	console.log("Hi");
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-	var file = files[0];
+   // Retrieve the first (and only!) File from the FileList object
+	var f = evt.target.files[0];
+	if (f) {
+		var r = new FileReader();
+		r.onload = function (e) {
+			console.log("Loading template")
+			readQuestionsFromDoc(e.target.result);
+			console.log("Loaded template")
+		}
+		r.readAsBinaryString(f)
+	} else {
+		console.log("Error reading file")
+	}
 	
-	output.push('<li><strong>', escape(file.name), '</strong> (', file.type || 'n/a', ') - ',
-			  file.size, ' bytes, last modified: ',
-			  file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a',
-			  '</li>');
-
-    document.getElementById('questions').innerHTML = '<ul>' + output.join('') + '</ul>';
   }
-
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+function readQuestionsFromDoc(file) {
+	setProcessingTemplate();
+	var zip = new JSZip(file);
+	var data = zip.files["word/document.xml"]._data.getContent();
+	var string = new TextDecoder("utf-8").decode(data);
+	var xml = string,
+	  xmlDoc = $.parseXML( xml ),
+	  $xml = $( xmlDoc );
+	console.log($xml)
+	var table = $xml.find('w\\:tbl,tbl').first();
+	console.log(table);
+	var rows = table.find('w\\:tr, tr');
+	console.log(rows);
+	var questions = [];
+	$.each(rows, function(key, row) {
+		debugger;
+		var elems = $(row).find('w\\:t, t')
+		if(elems.length === 3){
+			questions.push({question: elems[0].textContent, yesAnswer: elems[1].textContent, noAnswer: elems[2].textContent})
+		}
+	});
+	console.log(questions);
+	
+	return questions;
+}
+
+function setProcessingTemplate(){
+	
+}
